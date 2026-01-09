d45a1b71c5d8d8c75dc5dcd7fbbd0d2525c163fa
Carol Hoffmann <carolhoffmann@Carols-MacBook-Air.local>
2026-01-09 13:17:35 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
-export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-});
+// https://astro.build/config
+export default defineConfig({});
